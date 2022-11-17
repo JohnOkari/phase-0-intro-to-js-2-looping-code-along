487f0b2becc8c00733735a40d2f44584c5fd381d
JohnOkari <johnnokari@gmail.com>
2022-11-17 15:21:03 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(arrayString, eventName) {
+  let thanksCards = [];
+  for (let i = 0; i < arrayString.length; i++) {
+    thanksCards.push(
+      `Thank you, ${arrayString[i]}, for the wonderful ${eventName} gift!`
+    );
+  }
+  return thanksCards;
+}
+
+function countDown(positiveNumber) {
+  while (positiveNumber > 0) {
+    console.log(positiveNumber);
+    positiveNumber -= 1;
+  }
+  console.log(positiveNumber);
+}
